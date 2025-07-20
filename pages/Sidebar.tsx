3c6a1049f9e5da742a8b934bb6ea5a7499dc33fa
Ruturaj Marathe <marathe.rutu11@gmail.com>
2025-07-20 17:24:39 -0400

    Visteon Expereince added

--- pages/Sidebar.tsx
+++ pages/Sidebar.tsx
@@ -23,7 +23,7 @@ const Header = () => {
             </ScrollLink>
           </h1>
           <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-            Software Engineer
+            Software Engineer / Data Engineer
           </h2>
           <i> United States </i>
           <p className="mt-2 lg:max-w-sm leading-relaxed text-justify">
--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -19,6 +19,115 @@ const Experience = () => {
               <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  <header aria-label="2025 to 2025">
+                    June 2021 — August 2025
+                  </header>
+                  <Image
+                    alt=""
+                    loading="lazy"
+                    width="200"
+                    height="48"
+                    decoding="async"
+                    data-nimg="1"
+                    className="rounded transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                    style={{ color: "transparent" }}
+                    src={"/images/experiences/visteon.png"}
+                  />
+                </div>
+                <div className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    <div>
+                      <a
+                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                        href="https://www.visteon.com/products-technology/technology/default.aspx"
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="Application Management Intern at Visteon Corporation"
+                      >
+                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                        <span>
+                          Application Management Intern {" . "}
+                          <span className="inline-block">
+                            Visteon Corporation{" "}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                          </span>
+                        </span>
+                      </a>
+                    </div>
+                  </h3>
+                  <div className="mt-2 text-sm leading-normal text-justify">                
+                    <h3 className="font-bold">Project: vPay Migration</h3>
+                    <ul>
+                      <li>1. Engineered backend data flows to support the migration of 12+ finance reports from Qlik to Power BI, collaborating with analysts to improve usability and reporting speed.</li>
+                      <li>2. Built and orchestrated 2 Azure Data Factory ETL pipelines to automate Power BI deployments across 20+ global plants, maintaining 99% uptime and enabling proactive monitoring.</li>
+                      <li>3. Configured row-level security and access roles in Power BI for over 500 global users, ensuring compliance and secure enterprise-level access to financial data.</li>
+                      <li>4. Fixed defects in Oracle SQL stored procedures by handling date format mismatches, missing values in key financial columns, and erroneous data using DML and DDL operations.</li>
+                    </ul>
+                  </div>
+                  <ul
+                    className="mt-2 flex flex-wrap"
+                    aria-label="Technologies used"
+                  >
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Qlik
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        SQL Developer
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Oracle DB
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Power BI
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Azure Data Factory
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Azure Cloud
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Entreprise Engineereing Application
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Root Cause Analysis
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="mb-12">
+              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
                   <header aria-label="march 2024 to august 2024">
                     May 2024 — December 2024
                   </header>
