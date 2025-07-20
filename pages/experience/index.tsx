860e4c936f6fead89cd4df4cc91dd77839e7aead
Ruturaj Marathe <marathe.rutu11@gmail.com>
2025-07-20 17:30:10 -0400

    Visteon Expereince update

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -20,7 +20,7 @@ const Experience = () => {
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
                   <header aria-label="2025 to 2025">
-                    June 2021 — August 2025
+                    June 2025 — August 2025
                   </header>
                   <Image
                     alt=""
